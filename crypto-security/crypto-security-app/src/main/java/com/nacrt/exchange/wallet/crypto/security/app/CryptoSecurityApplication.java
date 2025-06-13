cf1c60f033c4ee74278883b7d259891da9634173
mars <zh@alicms.com>
2025-06-13 17:29:49 +0800

    fix

--- crypto-security/crypto-security-app/src/main/java/com/nacrt/exchange/wallet/crypto/security/app/CryptoSecurityApplication.java
+++ crypto-security/crypto-security-app/src/main/java/com/nacrt/exchange/wallet/crypto/security/app/CryptoSecurityApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -15,11 +14,6 @@ import org.springframework.context.annotation.PropertySource;
 })
 @EntityScan("com.nacrt.exchange.wallet.crypto.security.dao.model")
 @EnableJpaRepositories("com.nacrt.exchange.wallet.crypto.security.dao.repository")
-@PropertySource(value = {
-    "classpath:application.yml",
-    "classpath*:application.yml",
-    "classpath*:application-${spring.profiles.active}.yml"
-}, ignoreResourceNotFound = true)
 public class CryptoSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(CryptoSecurityApplication.class, args);
